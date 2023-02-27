26da80436b294c2a271da645d11cb5effa50b482
Nisrin Abou-Seido <nabouseido@gmail.com>
2023-02-27 18:24:35 -0500

    Added relevant packages and updated css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Calendar from './Calendar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Birthday Calendar</h3>
+        <div className="Calendar">
+          <Calendar />
+        </div>
     </div>
   );
 }
